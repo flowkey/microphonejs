d2368193f9a5265ac91d37793b642d2874d5e4f7
Ahmed Hassan <a.hassan@buismed.de>
2014-09-18 12:26:10 +0200

    added version for flashmic

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.2.2');
-  api.use(['underscore', 'deps', 'ui', 'flowkey:flashmic'], 'client');
+  api.use(['underscore', 'deps', 'ui', 'flowkey:flashmic@0.1.0'], 'client');
   api.addFiles(['src/audioresource.js', 'src/html5audio.js', 'src/flashaudio.js', 'src/microphone.js', ], 'client');
   api.export('Microphone');
 });
