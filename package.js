409f6109ec0d229349f50b587e913c33c89b4194
Ahmed Hassan <a.hassan@buismed.de>
2014-09-18 12:21:32 +0200

    updated package to 0.9.0

--- package.js
+++ package.js
@@ -1,16 +1,12 @@
 Package.describe({
-    summary: "Getting Microphone input from user"
+  summary: "Getting Microphone input from user",
+  version: "0.0.1",
+  git: "https://github.com/flowkey/microphonejs.git"
 });
 
-Package.on_use(function(api, where) {
-    api.use(['underscore', 'deps', 'ui', 'MicrophoneFlash'], 'client');
-    api.add_files(['src/audioresource.js', 'src/html5audio.js', 'src/flashaudio.js', 'src/microphone.js', ], 'client');
-
-    if (api.export)
-        api.export('Microphone');
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.2.2');
+  api.use(['underscore', 'deps', 'ui', 'flowkey:flashmic'], 'client');
+  api.addFiles(['src/audioresource.js', 'src/html5audio.js', 'src/flashaudio.js', 'src/microphone.js', ], 'client');
+  api.export('Microphone');
 });
-
-// Package.on_test(function(api) {
-//     api.use(['underscore', 'ejson', 'deps', 'settings-manager', 'ui', 'tinytest', 'test-helpers']);
-//     api.add_files(['tests/basictests.js'], 'client');
-// })
