b9e6d6543b0250682e50d3a21bc3e9aad472e07e
Ahmed Hassan <a.hassan@buismed.de>
2015-06-09 19:34:39 +0200

    minor refactoring

--- package.js
+++ package.js
@@ -7,13 +7,8 @@ Package.describe({
 Package.on_use(function(api, where) {
 	api.versionsFrom(['METEOR@0.9.0']);
     api.use(['underscore', 'deps', 'ui', 'flowkey:flashmic@0.1.2', 'flowkey:swfobject@1.0.0'], 'client');
-    api.add_files(['src/microphone.js', 'src/AudioResource.js', 'src/audioresources/html5audio.js', 'src/audioresources/flashaudio.js',], 'client');
+    api.add_files(['src/microphone.js', 'src/AudioResource.js', 'src/audioresources/html5audio.js', 'src/audioresources/flashaudio.js'], 'client');
 
     if (api.export)
         api.export('Microphone');
 });
-
-Package.on_test(function(api) {
-    api.use(['tinytest', 'test-helpers']);
-    api.add_files([], 'client');
-})
