8d5d68204ceffa7a219781322eb443f3946f6a92
rikner <rikner@web.de>
2016-06-23 16:12:39 +0200

    bumped version number to 1.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: 'Getting Microphone input from user and route it through web audio api chain',
-    version: '1.0.3',
+    version: '1.0.4',
     git: 'https://github.com/flowkey/microphonejs.git',
     name: 'flowkey:microphone'
 });
